08ebb7561e6a70dffc24134cb89565843daccc92
Yakir Uzan <yakiruzangreen@gmail.com>
2025-06-25 00:40:18 +0300

    added abstract class coalled shape with get area method and str method

---
+++ calculator.py
@@ -0,0 +1,12 @@
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+    def _init_(self, name="Shape"):
+        self.name = name
+
+    @abstractmethod
+    def get_area(self):
+        pass
+
+    def _str_(self):
+        return f"{self.name} with area: {self.get_area()}"
