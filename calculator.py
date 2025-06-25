ee8c2b5aaeb24a08136726247724ad226039039b
Yakir Uzan <yakiruzangreen@gmail.com>
2025-06-25 11:13:19 +0300

    I added an add method to the Shape class that connects 2 shapes and an eq method that compares 2 shapes and returns true or false, and I also added instances of the functions in the file.

--- calculator.py
+++ calculator.py
@@ -8,5 +8,17 @@ class Shape(ABC):
     def get_area(self):
         pass
 
-    def _str_(self):
+    def __str__(self):
         return f"{self.name} with area: {self.get_area()}"
+
+    #מתודת קסם שמחברת בין 2 צורות
+    def __add__(self,other):
+        if isinstance(other,Shape):
+            return self.get_area() + other.get_area()
+        return NotImplemented
+
+    #מתודת קסם שמשווה בין 2 צורות
+    def __eq__(self, other):
+        if isinstance(other, Shape):
+            return self.get_area() == other.get_area()
+        return NotImplemented
